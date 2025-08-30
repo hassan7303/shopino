460be0540b1664cb7b5eed4728e5fddfcf399857
hsnali <hassanali7303@gmail.com>
2025-08-30 21:51:16 +0330

    added Payment model

---
+++ app/Models/Payment.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    protected $fillable = [
+        'order_id',
+        'amount',
+        'method',
+        'status',
+    ];
+
+    protected $casts = [
+        'order_id' => 'integer',
+        'amount' => 'decimal:2',
+        'method' => 'string',
+        'status' => 'string',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+}
